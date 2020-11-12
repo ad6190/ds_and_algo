e23e2b74256c80df3c850b17059ad21db428c722
ad6190 <aditibhatnagar@finly.io>
2020-11-12 10:06:32 +0530

    edited factorial input

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,26 +1,14 @@
+import random 
 
 def factorial(n):
 	return n * factorial(n-1) if n > 1 else 1
 
 
 if __name__ == "__main__":
-	n = 10
+	n = random.randrange(-10, 10)
+	print(n)
 	if n < 0 :
 		print("invalid input") 
 		exit()
 	print(factorial(n))
-	n = 0
-	if n < 0 :
-		print("invalid input") 
-		exit()
-	print(factorial(n))
-	n = 5
-	if n < 0 :
-		print("invalid input") 
-		exit()
-	print(factorial(n))
-	n = -1
-	if n < 0 :
-		print("invalid input")
-		exit()  
-	print(factorial(n))
+	
